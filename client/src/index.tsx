95fa3106c689b24d71e39ba5d63d7f9dc20288bf
Reece Bedding <reecebedding@hotmail.co.uk>
2018-09-19 23:59:58 +0100

    Simplify base routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,14 @@ import * as  ReactDOM from "react-dom";
 import createHistory from "history/createBrowserHistory";
 import { Route, Switch, Router } from "react-router-dom";
 
-import { App } from "./components";
+import { Home } from "./components";
 
 import "../resources/scss/style.scss";
 
 ReactDOM.render(
   <Router history={createHistory()}>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact={true} path="/" component={Home} />
     </Switch>
   </Router>,
   document.getElementById("root"),
